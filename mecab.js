1bdb35b9440be6de0971dec502c97bd33d674913
ksmin23 <ksmin23@github.com>
2017-06-13 02:06:31 +0000

    coding style 정리

--- mecab.js
+++ mecab.js
@@ -1,7 +1,7 @@
 var cp = require('child_process');
 
 var MECAB_LIB_PATH =
-    process.env.MECAB_LIB_PATH ?
+  process.env.MECAB_LIB_PATH ?
     process.env.MECAB_LIB_PATH :
     __dirname + '/mecab';
 
@@ -31,59 +31,59 @@ var execMecab = function (text, callback, dicdir) {
 };
 
 var parseFunctions = {
-    'pos': function (result, elems) {
-        result.push([elems[0]].concat(elems[1].split(',')[0]));
-        return result;
-    },
+  'pos': function (result, elems) {
+    result.push([elems[0]].concat(elems[1].split(',')[0]));
+    return result;
+  },
 
-    'morphs': function (result, elems) {
-        result.push(elems[0]);
-        return result;
-    },
+  'morphs': function (result, elems) {
+    result.push(elems[0]);
+    return result;
+  },
 
-    'nouns': function (result, elems) {
-        var tag = elems[1].split(',')[0];
-        
-        if (tag === 'NNG' || tag === 'NNP') {
-            result.push(elems[0]);
-        }
+  'nouns': function (result, elems) {
+    var tag = elems[1].split(',')[0];
 
-        return result;
+    if (tag === 'NNG' || tag === 'NNP') {
+      result.push(elems[0]);
     }
+
+    return result;
+  }
 };
 
 var parse = function (text, method, callback, dicdir) {
-    execMecab(text, function (err, result) {
-        if (err) { return callback(err); }
+  execMecab(text, function (err, result) {
+    if (err) { return callback(err); }
 
-        result = result.split('\n').reduce(function(parsed, line) {
-            var elems = line.split('\t');
+    result = result.split('\n').reduce(function(parsed, line) {
+      var elems = line.split('\t');
 
-            if (elems.length > 1) {
-                return parseFunctions[method](parsed, elems);
-            } else {
-                return parsed;
-            }
-        }, []);
+      if (elems.length > 1) {
+        return parseFunctions[method](parsed, elems);
+      } else {
+        return parsed;
+      }
+    }, []);
 
-        callback(err, result);
-    }, dicdir);
+    callback(err, result);
+  }, dicdir);
 };
 
 var pos = function (text, callback, dicdir) {
-    parse(text, 'pos', callback, dicdir);
+  parse(text, 'pos', callback, dicdir);
 };
 
 var morphs = function (text, callback, dicdir) {
-    parse(text, 'morphs', callback, dicdir);
+  parse(text, 'morphs', callback, dicdir);
 };
 
 var nouns = function (text, callback, dicdir) {
-    parse(text, 'nouns', callback, dicdir);
+  parse(text, 'nouns', callback, dicdir);
 };
 
 module.exports = {
-    pos: pos,
-    morphs: morphs,
-    nouns: nouns
+  pos: pos,
+  morphs: morphs,
+  nouns: nouns
 };
